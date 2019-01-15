01755b89bd9a0747294fd897cd47a2ad5cc508b0
Derek Harmanli <derek.harmanli@gmail.com>
2019-01-15 12:55:49 -0500

    wazzup

--- bash.js
+++ bash.js
@@ -1,17 +1,5 @@
 process.stdout.write('prompt > ');
 
-process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim();
-    if (cmd === 'pwd') {
-        // process.stdout.write(__dirname);
-        process.stdout.write(process.cwd());
-    } else {
-        process.stdout.write('You typed: ' + cmd);
-    }
-    if (process.cwd() === __dirname) {
-        process.stdout.write('nice');
-    }
-    // process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
+const pwd = require('./pwd');
 
-});
+pwd();
